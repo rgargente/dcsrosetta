27cc02dba50cbff78902e61062d13281dc1c9906
rgargente <rgargente@gmail.com>
2018-10-02 23:49:16 +0100

    dict load and save

--- dcsrosettalib.py
+++ dcsrosettalib.py
@@ -1,4 +1,6 @@
+import json
 import os
+import tempfile
 import zipfile
 
 
@@ -16,9 +18,38 @@ def zip_mission(source_folder, dest_miz_path):
             zipf.write(file_path, file_path[len(source_folder):])
     zipf.close()
 
-    # zipf = zipfile.ZipFile(dest_miz_path, 'w', zipfile.ZIP_DEFLATED)
-    # for root, dirs, files in os.walk(source_folder):
-    #     for file in files:
-    #         # zipf.write(os.path.join(root, file))
-    #         zipf.write(file)
-    # zipf.close()
+
+def load_dict(dict_path):
+    with open(dict_path, encoding='UTF-8') as f:
+        s = f.read()
+    s = s.replace('\n', '')
+    s = s.replace('dictionary =', '')
+    s = s.replace('["', '"').replace('"]', '"')
+    s = s.replace('=', ':')
+    s = s.replace(',} -- end of dictionary', '}')
+    s = s.replace('\\', '\\\\')  # If we don't do this one slash ends up looking the same as two
+    return eval(s)
+
+
+def save_dict(dict: [], dest_path):
+    s = json.dumps(dict, indent=4, ensure_ascii=False)
+    s = 'dictionary = \n' + s
+    s = s.replace('"DictKey_', '["DictKey_').replace('": ', '"] = ')
+    s = s.replace('\\\\','\\\n')
+    s = s.replace('"\n}', '",\n} -- end of dictionary\n')
+    with open(dest_path, 'w', encoding='UTF-8') as f:
+        f.write(s)
+
+
+def translate(source_miz: str, dest_miz: str):
+    tmp = tempfile.mkdtemp()
+    unzip_mission(source_miz, tmp)
+
+    with open(os.path.join(tmp, 'l10n/DEFAULT/dictionary')) as f:
+        s = f.read()
+
+        # yaml_dict = yaml.safe_load(s)
+    # for i in yaml_dict:
+    #     print(i)
+
+    zip_mission(tmp, dest_miz)
--- test/test_dcsrosettalib.py
+++ test/test_dcsrosettalib.py
@@ -1,4 +1,5 @@
 import os
+import filecmp
 
 import dcsrosettalib
 
@@ -10,4 +11,27 @@ def test_unzip_and_zip(tmpdir):
     new_path = os.path.join(tmpdir, '01.miz')
     dcsrosettalib.zip_mission(unzip_folder, new_path)
 
-    # assert filecmp.cmp(source_miz, new_path) # We should do something here but for now I just checked it is ok manually
+    # We should do something here but for now I just checked it is ok manually
+    # assert filecmp.cmp(source_miz, new_path)
+
+
+def test_load_dict(tmpdir):
+    old_dict_path = 'dictionary'
+    dict = dcsrosettalib.load_dict(old_dict_path)
+    new_dict_path = os.path.join(tmpdir, 'newdict.txt')
+    dcsrosettalib.save_dict(dict, new_dict_path)
+
+    with open(old_dict_path, encoding='UTF-8') as f:
+        old_content = f.read()
+    with open(new_dict_path, encoding='UTF-8') as f:
+        new_content = f.read()
+    assert old_content.replace(" ", "") == new_content.replace(" ", "")
+
+
+
+
+def test_translate_miz(tmpdir):
+    source_miz = 'Pilotenalltag/Pilotenalltag_01.miz'
+    dest_mix = os.path.join(tmpdir, '01.miz')
+    dcsrosettalib.translate(source_miz, dest_mix)
+    assert False
